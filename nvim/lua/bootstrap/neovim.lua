4df38c5d30db9327c359d04e9755bbb991de5f95
Sahib <sahib.alejandro@gmail.com>
2024-01-04 21:34:37 -0600

    Added more general neovim settings

--- nvim/lua/bootstrap/neovim.lua
+++ nvim/lua/bootstrap/neovim.lua
@@ -9,10 +9,29 @@ vim.o.scrolloff = 5
 vim.o.sidescrolloff = vim.o.scrolloff
 vim.o.mouse = ""
 
+-- Splits that make more sense
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+-- Confirm instead of throwing a big ugly error
+vim.o.confirm = true
+
+-- Make wildmenu behave more friendly
+vim.o.wildmode = "longest:full,full"
+
+-- Set the title so tmux can display more
+-- info in its "pane_title" setting.
+vim.o.title = true
+
+-- Configure indentation
 vim.o.tabstop = 2
 vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.shiftwidth = vim.o.tabstop
+
+-- Make invisible characters visible
+vim.o.list = true
+vim.o.listchars = "tab:> ,trail:·"
 
 -- Search
 vim.o.ignorecase = true
