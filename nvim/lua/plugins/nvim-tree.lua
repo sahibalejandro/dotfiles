b88edb90b5d220d8e8c6a808bbef6ef8ffc6b0ce
Sahib <sahib.alejandro@gmail.com>
2024-01-10 01:00:24 -0600

    Styling Nvim Tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -2,17 +2,37 @@ return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
 
+  init = function()
+    -- Disable netrw to not interfere with Nvim Tree
+    -- See :help nvim-tree-netrw
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+  end,
+
   config = function()
     require("nvim-tree").setup({
-      view = {
-        number = true,
-        relativenumber = true,
-      },
-
       actions = {
         open_file = {
           quit_on_open = true,
         },
+      },
+
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          show = {
+            folder_arrow = false,
+          },
+        },
+      },
+
+      view = {
+        width = 35,
+        number = true,
+        signcolumn = "auto",
+        relativenumber = true,
       },
     })
   end,
