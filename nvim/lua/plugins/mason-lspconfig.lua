06628c231b7cc7c082f8b0a0851b1dc4409e770f
Sahib <sahib.alejandro@gmail.com>
2024-01-16 00:05:01 -0600

    LSP updates: disable virtual text, show source in float window.

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,17 +1,17 @@
 return {
-  "williamboman/mason-lspconfig.nvim",
+  'williamboman/mason-lspconfig.nvim',
 
   dependencies = {
-    "williamboman/mason.nvim",
-    "neovim/nvim-lspconfig",
-    "hrsh7th/cmp-nvim-lsp",
+    'williamboman/mason.nvim',
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
   },
 
   config = function()
-    require("mason-lspconfig").setup()
+    require('mason-lspconfig').setup()
 
-    local lspconfig = require("lspconfig")
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local lspconfig = require('lspconfig')
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
     local default_capabilities = { capabilities = capabilities }
 
     lspconfig.tailwindcss.setup(default_capabilities)
@@ -33,7 +33,7 @@ return {
       -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tsserver
       root_dir = function(filename)
         local flowRootDir = lspconfig.util.root_pattern('.flowconfig')(filename)
-        local tsRootDir = lspconfig.util.root_pattern("tsconfig.json", "package.json", "jsconfig.json", ".git")(filename)
+        local tsRootDir = lspconfig.util.root_pattern('tsconfig.json', 'package.json', 'jsconfig.json', '.git')(filename)
 
         if flowRootDir == nil then
           return tsRootDir
@@ -43,18 +43,24 @@ return {
       end
     })
 
-    -- Global Keymaps
-    vim.keymap.set("n", "<leader>lo", vim.diagnostic.open_float)
+    -- Diagnostic Configurations
+    vim.diagnostic.config({
+      virtual_text = false,
+      float = {
+        source = true,
+      },
+    })
 
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    -- LSP keymaps
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
       callback = function(event)
-        -- Buffer local mappings
         local options = { buffer = event.buf }
-        vim.keymap.set("n", "<leader>lt", vim.lsp.buf.type_definition, options)
-        vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, options)
-        vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, options)
-        vim.keymap.set("n", "<leader>li", vim.lsp.buf.hover, options)
+        vim.keymap.set('n', '<leader>lo', vim.diagnostic.open_float, options)
+        vim.keymap.set('n', '<leader>lt', vim.lsp.buf.type_definition, options)
+        vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, options)
+        vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, options)
+        vim.keymap.set('n', '<leader>li', vim.lsp.buf.hover, options)
       end,
     })
   end,
