0049cd21897a5e7efc3c10c5e26e5aa81dbdc671
Sahib <sahib.alejandro@gmail.com>
2024-01-17 20:33:46 -0600

    LSP: Added cmp-nvim-lsp-signature-help & lspkind

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,37 +1,46 @@
 return {
-  "hrsh7th/nvim-cmp",
+  'hrsh7th/nvim-cmp',
 
   dependencies = {
-    "SirVer/ultisnips",
-    "quangnguyen30192/cmp-nvim-ultisnips",
+    'SirVer/ultisnips',
+    'quangnguyen30192/cmp-nvim-ultisnips',
+    'hrsh7th/cmp-nvim-lsp-signature-help',
+    'onsails/lspkind-nvim',
   },
 
   config = function()
-    local cmp = require("cmp")
+    local cmp = require('cmp')
+    local lspkind = require('lspkind')
 
     cmp.setup({
       snippet = {
         expand = function(args)
-          vim.fn["UltiSnips#Anon"](args.body)
+          vim.fn['UltiSnips#Anon'](args.body)
         end,
+      },
+
+      formatting = {
+        format = lspkind.cmp_format(),
       },
 
       sources = cmp.config.sources({
         { name = 'ultisnips' },
-        { name = "nvim_lsp" },
+        -- nvim_lsp source is added as a dependency of mason-lspconfig.
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lsp_signature_help' },
       }, {
-        name = "buffer",
+        name = 'buffer',
       }),
 
       mapping = cmp.mapping.preset.insert({
-        ["<c-j>"] = cmp.mapping.select_next_item(),
-        ["<c-k>"] = cmp.mapping.select_prev_item(),
-        ["<c-n>"] = cmp.mapping.scroll_docs(1),
-        ["<c-p>"] = cmp.mapping.scroll_docs(-1),
-        ["<c-k>"] = cmp.mapping.select_prev_item(),
-        ["<tab>"] = cmp.mapping.confirm({ select = true }),
-        ["<cr>"] = cmp.mapping.confirm({ select = true }),
-        ["<esc>"] = cmp.mapping.abort(),
+        ['<c-j>'] = cmp.mapping.select_next_item(),
+        ['<c-k>'] = cmp.mapping.select_prev_item(),
+        ['<c-n>'] = cmp.mapping.scroll_docs(1),
+        ['<c-p>'] = cmp.mapping.scroll_docs(-1),
+        ['<c-k>'] = cmp.mapping.select_prev_item(),
+        ['<tab>'] = cmp.mapping.confirm({ select = true }),
+        ['<cr>'] = cmp.mapping.confirm({ select = true }),
+        ['<esc>'] = cmp.mapping.abort(),
       }),
     })
   end,
