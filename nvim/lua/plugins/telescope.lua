54dd8a895f0f374d2303b1c3277fb55a70c2d64b
Sahib <sahib.alejandro@gmail.com>
2024-06-26 01:13:57 -0600

    Adding Neovim Plugin: Telescope

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  branch = '0.1.x',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+
+  config = function()
+    require('telescope').setup {}
+  end,
+
+  keys = {
+    { '<leader>r', require('telescope.builtin').resume },
+    { '<leader>o', require('telescope.builtin').oldfiles },
+    { '<leader>s', require('telescope.builtin').live_grep },
+    { '<leader>f', require('telescope.builtin').find_files },
+    { '<leader>k', require('telescope.builtin').grep_string },
+  },
+}
