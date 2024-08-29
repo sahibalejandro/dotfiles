7f8e0fa8002b433fb93f9e078679c722f6853e2f
Sahib Alejandro Jaramillo Leo <sahib.jaramillo@improving.com>
2024-08-29 15:11:36 -0600

    Update wezterm settings: opacity, harfbuzz_features

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,13 +7,18 @@ config.color_scheme = '3024 (base16)'
 -- Fonts
 config.font_size = 14
 config.line_height = 1.5
+config.harfbuzz_features = {
+  'calt=0',
+  'clig=0',
+  'liga=0',
+}
 
 -- Wezterm Apperance
 config.hide_tab_bar_if_only_one_tab = true
 
 -- Window
 config.window_decorations = 'RESIZE'
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.90
 config.macos_window_background_blur = 20
 
 return config
