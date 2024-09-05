ce0f6150d53a202c0fc20dc1b92e4642faed5ca6
Sahib <sahib.alejandro@gmail.com>
2024-09-05 17:07:25 -0600

    Set wezterm font family

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,13 +5,17 @@ local config = wezterm.config_builder()
 config.color_scheme = '3024 (base16)'
 
 -- Fonts
-config.font_size = 14
-config.line_height = 1.5
+config.font = wezterm.font'SFMono Nerd Font Mono'
+config.font_size = 15
+config.line_height = 1.6
+
+--[[ Disable Font Ligatures if needed:
 config.harfbuzz_features = {
   'calt=0',
   'clig=0',
   'liga=0',
 }
+]]--
 
 -- Wezterm Apperance
 config.hide_tab_bar_if_only_one_tab = true
