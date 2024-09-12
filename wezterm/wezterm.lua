3fb7b8db1f9c12aaae29c47f0d06d8b535c189a4
Sahib <sahib.alejandro@gmail.com>
2024-09-12 00:32:16 -0600

    Added TokyoNight theme for WezTerm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,30 +2,19 @@ local wezterm = require'wezterm'
 local config = wezterm.config_builder()
 
 -- Colors
-config.color_scheme = '3024 (base16)'
-config.colors = {
-  background = '#080808'
-}
+config.color_scheme = 'tokyonight_night'
 
 -- Fonts
 config.font = wezterm.font'SFMono Nerd Font Mono'
 config.font_size = 15
 config.line_height = 1.6
 
---[[ Disable Font Ligatures if needed:
-config.harfbuzz_features = {
-  'calt=0',
-  'clig=0',
-  'liga=0',
-}
-]]--
-
 -- Wezterm Apperance
 config.hide_tab_bar_if_only_one_tab = true
 
 -- Window
 config.window_decorations = 'RESIZE'
-config.window_background_opacity = 0.95
-config.macos_window_background_blur = 20
+config.window_background_opacity = 0.98
+config.macos_window_background_blur = 10
 
 return config
