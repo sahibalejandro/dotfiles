6449108a905ddd01c429b850f38eadd636e4f66b
Sahib <sahib.alejandro@gmail.com>
2024-12-28 22:23:26 -0600

    Minimalize Telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,60 +1,41 @@
--- Wrap telescope functions to avoid loading Telescope
--- during startup and make this plugin real lazy
-function resume()
-  require'telescope.builtin'.resume()
-end
-
-function old_files()
-  require'telescope.builtin'.oldfiles()
-end
-
-function live_grep()
-  require'telescope.builtin'.live_grep()
-end
-
-function find_files()
-  require'telescope.builtin'.find_files()
-end
-
-function grep_string()
-  require'telescope.builtin'.grep_string()
-end
-
-function lsp_references()
-  require'telescope.builtin'.lsp_references()
-end
-
-function lsp_definitions()
-  require'telescope.builtin'.lsp_definitions()
-end
-
 return {
-  'nvim-telescope/telescope.nvim',
+  "nvim-telescope/telescope.nvim",
 
-  branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  lazy = true,
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make"
+    },
+  },
 
   config = function()
-    require'telescope'.setup{
+    require("telescope").setup {
       defaults = {
-        layout_strategy = 'vertical',
+        layout_strategy = "vertical",
         layout_config = {
-          width = 0.9,
-          preview_cutoff = 30,
-          preview_height = 0.6,
-        },
-      },
+          width = { padding = 0 },
+          height = { padding = 0 },
+          preview_cutoff = 10,
+        }
+      }
     }
+
+    local builtin = require "telescope.builtin"
+
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+    vim.keymap.set("n", "<leader>lg", builtin.live_grep, { desc = "Telescope live grep" })
+    vim.keymap.set("n", "<leader>of", builtin.oldfiles, { desc = "Telescope old files" })
+    vim.keymap.set("n", "<leader>gs", builtin.grep_string, { desc = "Telescope grep string" })
+    vim.keymap.set("n", "<leader>ht", builtin.help_tags, { desc = "Telescope help tags" })
   end,
 
   keys = {
-    { '<leader>r', resume  },
-    { '<leader>o', old_files },
-    { '<leader>s', live_grep },
-    { '<leader>f', find_files },
-    { '<leader>k', grep_string },
-    { '<leader>lr', lsp_references },
-    { '<leader>ld', lsp_definitions },
-  },
+    "<leader>ff",
+    "<leader>lg",
+    "<leader>of",
+    "<leader>gs",
+    "<leader>ht",
+  }
 }
