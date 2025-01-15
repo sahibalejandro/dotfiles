d934da17fe5b06b8e3c6d8e808bca05169e6ec42
Sahib <sahib.alejandro@gmail.com>
2025-01-14 22:14:33 -0600

    Set treesitter ensure_installed list

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,26 @@ return {
 
   config = function()
     require("nvim-treesitter.configs").setup {
+      ensure_installed = {
+        "bash",
+        "css",
+        "dockerfile",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "php",
+        "prisma",
+        "rust",
+        "scss",
+        "sql",
+        "tmux",
+        "tsx",
+        "typescript",
+        "vim",
+        "vue",
+        "yaml",
+      },
       highlight = {
         enable = true,
       },
