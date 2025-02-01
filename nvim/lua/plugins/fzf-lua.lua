fe33f71d22da4e2b0e6796aeccad243bb7d40c2b
Sahib <sahib.alejandro@gmail.com>
2025-02-01 00:50:01 -0600

    Use FZF Lua instead of Telescope

---
+++ nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,30 @@
+return {
+  'ibhagwan/fzf-lua',
+
+  config = function()
+    local fzf_lua = require("fzf-lua")
+
+    fzf_lua.setup({
+      winopts = {
+        fullscreen = true,
+      },
+      oldfiles = {
+        include_current_session = true,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>ff", fzf_lua.files, { desc = "Find files" })
+    vim.keymap.set("n", "<leader>of", fzf_lua.oldfiles, { desc = "Find old files" })
+    vim.keymap.set("n", "<leader>gw", fzf_lua.grep_cword, { desc = "Grep word under cursor" })
+    vim.keymap.set("n", "<leader>lg", fzf_lua.live_grep, { desc = "Live grep" })
+    vim.keymap.set("n", "<leader>lk", fzf_lua.keymaps, { desc = "List keymaps" })
+  end,
+
+  keys = {
+    "<leader>ff",
+    "<leader>of",
+    "<leader>gw",
+    "<leader>lg",
+    "<leader>lk",
+  },
+}
