139caecff8862a8e2da2c103c56b516dc40a9607
Sahib <sahib.alejandro@gmail.com>
2025-02-18 22:20:35 -0600

    Run linters on BufReadPost and InsertLeave

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -8,7 +8,7 @@ return {
       scss = {'stylelint'}
     }
 
-    vim.api.nvim_create_autocmd({'BufWritePost'}, {
+    vim.api.nvim_create_autocmd({ 'BufReadPost', 'InsertLeave' }, {
       callback = function()
         lint.try_lint()
       end
