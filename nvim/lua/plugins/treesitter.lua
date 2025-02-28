5459a0b81824d268c2acc5764d3f115e1f17cf43
Sahib <sahib.alejandro@gmail.com>
2025-02-27 23:38:20 -0600

    Update Nvim Treesitter config to avoid parser errors after updates

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,26 @@
 return {
   "nvim-treesitter/nvim-treesitter",
 
+  build = ":TSUpdate",
+
   config = function()
     require("nvim-treesitter.configs").setup {
       ensure_installed = {
+        -- These parsers are bundled in NeoVim since v0.9.0, by adding them
+        -- to this list we ensure that these parsers are taken from Nvim
+        -- Treesitter and they are updated correctly when running :TSUpdate
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "markdown",
+
+        -- These parsers are not bundled in NeoVim.
         "bash",
         "css",
         "html",
         "javascript",
         "json",
-        "lua",
         "php",
         "prisma",
         "rust",
@@ -18,7 +29,6 @@ return {
         "tmux",
         "tsx",
         "typescript",
-        "vim",
         "vue",
         "yaml",
       },
