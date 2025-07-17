6a4a35cd9291e34ebaa519b910eb34a24619c3d7
Sahib <sahib.alejandro@gmail.com>
2025-07-16 23:15:46 -0600

    [nvim] small ui updates

--- nvim/lua/neovim.lua
+++ nvim/lua/neovim.lua
@@ -9,11 +9,12 @@ vim.o.smartcase = true
 vim.o.signcolumn = 'yes'
 vim.o.wrap = false
 vim.o.swapfile = false
-vim.cmd [[set completeopt+=menuone,noselect]]
+vim.o.winborder = 'single'
 
 vim.keymap.set('i', 'jk', '<esc>')
-vim.keymap.set('n', '<space><space>', ':b#<cr>')
+vim.keymap.set('n', '<space><space>', '<c-^>')
 vim.keymap.set('n', '<space>n', ':cn<cr>')
 vim.keymap.set('n', '<space>p', ':cp<cr>')
 
 vim.cmd [[colorscheme retrobox]]
+vim.cmd [[set completeopt+=menuone,noinsert,fuzzy]]
