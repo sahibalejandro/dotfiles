194e16d696f7058b52bf1f7706a09fc2d2aabfdc
Sahib <sahib.alejandro@gmail.com>
2025-07-24 21:52:34 -0600

    [nvim] Enabled LSP auto completion

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,1 +1,9 @@
+vim.lsp.config('*', {
+  on_attach = function(client, bufnr)
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
+    end
+  end,
+})
+
 vim.lsp.enable('php')
