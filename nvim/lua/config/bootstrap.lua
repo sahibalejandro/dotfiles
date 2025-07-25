716a259f7f7f2901e4deb2660e0620f8d54f3cf0
Sahib <sahib.alejandro@gmail.com>
2025-07-25 00:02:00 -0600

    Added S command for searching using rg

--- nvim/lua/config/bootstrap.lua
+++ nvim/lua/config/bootstrap.lua
@@ -17,5 +17,9 @@ vim.o.grepprg = 'rg --vimgrep'
 vim.keymap.set('i', 'jk', '<esc>')
 vim.keymap.set('n', '<space><space>', '<c-^>')
 
+vim.api.nvim_create_user_command('S', function(opts)
+  vim.cmd('sil gr '..opts.args..'|copen')
+end, { nargs = 1, desc = 'Search using grep' })
+
 vim.cmd('colorscheme habamax')
 vim.cmd('set completeopt+=menuone,noinsert,fuzzy')
