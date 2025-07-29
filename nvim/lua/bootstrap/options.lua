bda61307e59c716064f3e9e949a84fe88a5ba669
Sahib <sahib.alejandro@gmail.com>
2025-07-28 23:55:57 -0600

    [nvim] Enable LSP autocompletion

--- nvim/lua/bootstrap/options.lua
+++ nvim/lua/bootstrap/options.lua
@@ -8,9 +8,9 @@ vim.o.softtabstop = -1
 vim.o.hlsearch = false
 vim.o.ignorecase = true
 vim.o.smartcase = true
+vim.o.winborder = 'rounded'
 
 vim.keymap.set('i', 'jk', '<esc>')
 vim.keymap.set('n', '<leader><leader>', '<c-^>')
 
-vim.cmd('colorscheme habamax')
 vim.cmd('highlight Normal guibg=None')
