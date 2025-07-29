212b082cbf042d1f6b5cf565b557bb64808eb9a3
Sahib <sahib.alejandro@gmail.com>
2025-07-28 20:47:59 -0600

    [nvim] Remove netrw FileType autocmd

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,10 +1,3 @@
 vim.keymap.set('i', 'jk', '<esc>')
 vim.keymap.set('n', '<leader><leader>', '<c-^>')
 vim.keymap.set('n', '<leader>e', ':Ex<cr>')
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'netrw',
-  callback = function(ev)
-    vim.keymap.set('n', '<esc>', ':bwipeout<cr>', { buffer = true })
-  end,
-})
