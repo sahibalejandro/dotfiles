7a57887ea44cef39e340bd6443ae11e56ef20793
Sahib <sahib.alejandro@gmail.com>
2025-07-29 00:25:11 -0600

    [nvim] Setup netrw

--- nvim/lua/bootstrap/options.lua
+++ nvim/lua/bootstrap/options.lua
@@ -9,8 +9,10 @@ vim.o.hlsearch = false
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.winborder = 'rounded'
+vim.g.netrw_banner = false
 
 vim.keymap.set('i', 'jk', '<esc>')
 vim.keymap.set('n', '<leader><leader>', '<c-^>')
+vim.keymap.set('n', '<leader>e', '<cmd>Ex<cr>')
 
 vim.cmd('highlight Normal guibg=None')
