2b7cad87b35f80bf3e53df08270478543599f560
Sahib <sahib.alejandro@gmail.com>
2025-07-30 23:51:17 -0600

    [tmux][nvim] Change status bars style

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,9 +1,13 @@
 vim.cmd('colorscheme tokyonight-night')
 
+-- Neovim groups
 vim.cmd('hi Normal guibg=none')
 vim.cmd('hi SignColumn guibg=none')
 vim.cmd('hi LineNr guifg=#a9b1d6')
 vim.cmd('hi LineNrAbove guifg=#a9b1d6')
 vim.cmd('hi LineNrBelow guifg=#a9b1d6')
 vim.cmd('hi EndOfBuffer guifg=#a9b1d6')
+vim.cmd('hi StatusLine guibg=none')
+
+-- Plugin groups
 vim.cmd('hi FzfLuaNormal guibg=none')
