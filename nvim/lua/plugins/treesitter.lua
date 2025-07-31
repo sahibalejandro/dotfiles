3ff50e4193b0d8a39fc59e750cc6a89d3568b7de
Sahib <sahib.alejandro@gmail.com>
2025-07-30 22:49:10 -0600

    [nvim] Setup nvim-treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,5 +2,33 @@ return {
   "nvim-treesitter/nvim-treesitter",
   branch = 'master',
   lazy = false,
-  build = ":TSUpdate"
+  build = ":TSUpdate",
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      highlight = {
+        enable = true,
+      },
+      ensure_installed = {
+        'bash',
+        'css',
+        'dockerfile',
+        'html',
+        'javascript',
+        'json',
+        'lua',
+        'markdown',
+        'markdown_inline',
+        'php',
+        'rust',
+        'scss',
+        'sql',
+        'tmux',
+        'toml',
+        'tsx',
+        'typescript',
+        'vim',
+        'yaml',
+      },
+    })
+  end,
 }
