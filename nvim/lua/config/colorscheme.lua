10e28325448ab2d4355d9f36b8e4aba1beef6ded
Sahib <sjaramillo@etsy.com>
2025-09-05 15:16:41 -0600

    [tmux][nvim][alacritty] fix colors

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -5,9 +5,9 @@ vim.cmd('hi Normal guibg=none')
 vim.cmd('hi NormalNC guibg=none')
 vim.cmd('hi SignColumn guibg=none')
 vim.cmd('hi LineNr guifg=#a9b1d6')
-vim.cmd('hi LineNrAbove guifg=#666e8f')
-vim.cmd('hi LineNrBelow guifg=#666e8f')
-vim.cmd('hi EndOfBuffer guifg=#666e8f')
+vim.cmd('hi LineNrAbove guifg=#676e8c')
+vim.cmd('hi LineNrBelow guifg=#676e8c')
+vim.cmd('hi EndOfBuffer guifg=#676e8c')
 
 -- Plugin groups
 vim.cmd('hi FzfLuaNormal guibg=none')
