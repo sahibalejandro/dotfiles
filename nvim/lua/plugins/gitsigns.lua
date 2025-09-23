eb432c1eeea39534f1f9a16f8e304b0bf83cbab0
Sahib <sjaramillo@etsy.com>
2025-09-23 22:30:03 +0000

    [nvim] Set gitsigns blame formatter

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,7 +2,9 @@ return {
   'lewis6991/gitsigns.nvim',
   config = function()
     local gitsigns = require('gitsigns')
-    gitsigns.setup()
+    gitsigns.setup({
+      current_line_blame_formatter = '<abbrev_sha> <author> <author_time:%R> - <summary>',
+    })
 
     vim.keymap.set('n', '<leader>b', gitsigns.toggle_current_line_blame)
   end
