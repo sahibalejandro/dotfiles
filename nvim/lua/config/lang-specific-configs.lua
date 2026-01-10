e6abfea8ed2d87599ee5caac03e97f56df2700d6
Sahib <sahib.alejandro@gmail.com>
2026-01-09 22:09:29 -0600

    [nvim] Indent PHP files on BufNewFile

--- nvim/lua/config/lang-specific-configs.lua
+++ nvim/lua/config/lang-specific-configs.lua
@@ -2,7 +2,7 @@
 -- You can add multiple auto commands as needed.
 
 -- Change PHP indentation to 4 spaces.
-vim.api.nvim_create_autocmd('BufRead', {
+vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
   pattern = '*.php',
   callback = function(ev)
     vim.bo[ev.buf].shiftwidth = 4
