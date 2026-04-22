1a78ce04d51f9f8bd61c0589eafdc4ff57d7f0f4
Sahib <sahib.alejandro@gmail.com>
2026-04-21 19:46:44 -0600

    [nvim] Use nvim-treesitter from main branch

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,53 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = 'master',
+  branch = 'main',
   lazy = false,
   build = ":TSUpdate",
   config = function()
-    require('nvim-treesitter.configs').setup({
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      ensure_installed = {
-        'bash',
-        'css',
-        'dockerfile',
-        'html',
-        'javascript',
-        'json',
-        'lua',
-        'markdown',
-        'markdown_inline',
-        'php',
-        'rust',
-        'scss',
-        'sql',
-        'tmux',
-        'toml',
-        'tsx',
-        'typescript',
-        'vim',
-        'yaml',
-      },
+    local nvim_treesitter = require('nvim-treesitter')
+
+    -- Automatically install parser for my common programming languages
+    nvim_treesitter.install({
+      'bash',
+      'css',
+      'dockerfile',
+      'html',
+      'javascript',
+      'json',
+      'lua',
+      'markdown',
+      'markdown_inline',
+      'php',
+      'rust',
+      'scss',
+      'sql',
+      'tmux',
+      'toml',
+      'tsx',
+      'typescript',
+      'vim',
+      'vue',
+      'yaml',
+    })
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { '*' },
+      callback = function(ev)
+        local filetype = vim.bo[ev.buf].filetype
+        local installed_parsers = nvim_treesitter.get_installed()
+
+        if not vim.tbl_contains(installed_parsers, filetype) then
+          vim.notify("Treesitter parser for " .. filetype .. " is not installed. Run :TSInstall " .. filetype, vim.log.levels.WARN)
+        else
+          -- syntax highlighting, provided by Neovim
+          vim.treesitter.start()
+          -- folds, provided by Neovim
+          -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          -- vim.wo.foldmethod = 'expr'
+          -- indentation, provided by nvim-treesitter
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
     })
   end,
 }
